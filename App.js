0fd649591c39e8f231459bdffd5875d2a60e3e93
Colemar Araújo Aguiar <colemaraaguiar@gmail.com>
2021-03-22 02:17:00 -0300

    Front start
    Alterei o caminho das imagens e adicionei as imagens do menu principal-funcionário no código.

--- App.js
+++ App.js
@@ -1,112 +1,96 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function App(){
+  return(
+    
+    <View style={styles.centralMenuFun}>
+        
+        <View >
+          <Text style={styles.soum}>sacana</Text>
+          
+          <Image
+           source={require('./android/src/assets/bsmlogo.png')}
+            
+           style={styles.logoStyle}
+           
+             />
+          <Image
+            source={require('./android/src/assets/menu.png')}
+          
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          />
+          
+          <Text style={styles.soum}>isso aí</Text>
+        
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+        
+        <View>
+        
+       
+       <Text style={styles.soum}>Só um teste boy</Text>
+       
+       <Image
+        source={require('./android/src/assets/servico1.png')}
+      
 
+        style={styles.backgroundservices}
+        />
+        <Image
+        source={require('./android/src/assets/servico2.png')}
+      
+
+        style={styles.backgroundservices}
+        />
+        <Image
+        source={require('./android/src/assets/servico1.png')}
+      
+
+        style={styles.backgroundservices}
+        />
+        <Image
+        source={require('./android/src/assets/servico2.png')}
+      
+
+        style={styles.backgroundservices}
+        />
+        
+        <Text style={styles.soum}>brbr</Text>
+        
+        </View>
+        
+        
+      
+      </View>
+     )
+    
+}
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+    centralMenuFun:{
+      flex:1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#E5E5E5'
+    },
+    
+    logoStyle:{
+     width:60,
+     height:56,
+    
+    },
+    soum:{
+      fontSize:20,
+      color:'#FF0000'
 
-export default App;
+    },
+    backgroundservices:{
+      backgroundColor:'#545454',
+      borderRadius:10,
+      width:60,
+      height:56
+      
+      
+    }
+   
+
+}); 
