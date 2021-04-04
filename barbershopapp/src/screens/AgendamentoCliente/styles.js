038d69320699102df6690f0311bf23e4406bbc62
Colemar Araújo Aguiar <colemaraaguiar@gmail.com>
2021-04-04 17:21:14 -0300

    Add all the screens from client

---
+++ barbershopapp/src/screens/AgendamentoCliente/styles.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+    background-color: #C3C3C3;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
---
+++ barbershopapp/src/screens/NotificaçãoPagamento/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native'
+import { Text, View } from 'react-native'
+import { 
+    Container,
+    InputArea,
+    CustomButton,
+    CustomButtonText,
+    SignMessageButton,
+    SignMessageButtonText,
+    SignMessageButtonTextBold,
+    LogIn,
+    Arrumar,
+   
+
+    
+    
+} from './styles';
+
+
+import BarberLogo from '../../assets/barberlogo.svg';
+
+
+export default () => {
+    return (
+      
+        <Container>
+            <View><Text>Notificação do pagamento Cliente</Text></View>
+            <BarberLogo height="245"/>
+            
+        </Container>
+        
+    );
+}
